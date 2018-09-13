1322e86fd6790be2bf05b969ce233c6a94f37a61
Matthew Phillips <Matthew.Phillips@defra.gsi.gov.uk>
2018-09-13 16:40:45 +0100

    changed namespace, made exception serialiasable

--- src/ICAPNetClient/ICAPException.cs
+++ src/ICAPNetClient/ICAPException.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
-namespace ICAPNameSpace
+namespace ICAPNetClient
 {
     /// <summary>
     /// Basic Exception to use in relation with ICAP
     /// </summary>
+    [Serializable]
     public class ICAPException : Exception
     {
+        public ICAPException()
+        {
+        }
+
         public ICAPException(string message)
             : base(message)
+        {
+        }
+
+        public ICAPException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        // Without this constructor, deserialization will fail
+        protected ICAPException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
--- src/ICAPNetClient/ICAPNetClient.cs
+++ src/ICAPNetClient/ICAPNetClient.cs
@@ -1,4 +1,4 @@
-﻿namespace ICAPNameSpace
+﻿namespace ICAPNetClient
 {
     using System;
     using System.Collections.Generic;
